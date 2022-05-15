e007aad319dff9c1c3ec0f340496cd1a9b1f4cd1
rida el mazary <rida.el99@gmail.com>
2022-05-15 16:35:21 +0100

    ft_atoi_long

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -36,3 +36,28 @@ int	ft_atoi(const char *str)
 	}
 	return (sign * ret);
 }
+
+long	ft_atoi_long(const char *str)
+{
+	int		i;
+	int		sign;
+	long	ret;
+
+	ret = 0;
+	i = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		ret = ret * 10 + (str[i] - 48);
+		i++;
+	}
+	return (sign * ret);
+}
