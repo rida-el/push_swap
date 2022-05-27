71fbca7ea3cb2fba2639c67a279efc72413db40b
rida el mazary <rida.el99@gmail.com>
2022-05-27 20:07:38 +0100

    split function and move helper functions to sep files

--- bonus/main.c
+++ bonus/main.c
@@ -1,82 +1,7 @@
 #include "../push_swap.h"
 
-int	is_valid_individual_input(char *input)
+void	exec_one_instruction(t_stack *a, t_stack *b, char *instruction)
 {
-	int		i;
-	long	num;
-
-	i = 1;
-	if (!(ft_isdigit(input[0]) || input[0] == '+' || input[0] == '-' ))
-		return (0);
-	while (input[i])
-	{
-		if (!ft_isdigit(input[i]))
-			return (0);
-		i++;
-	}
-	if (i > 11)
-		return (0);
-	num = ft_atoi_long(input);
-	if (num > 2147483647 || num < -2147483648)
-		return (0);
-	return (1);
-}
-
-int	is_invalid_input(char **split)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (split[i])
-	{
-		if (!is_valid_individual_input(split[i]))
-			return (0);
-		j = i + 1;
-		while (split[j])
-		{
-			if (ft_atoi_long(split[i]) == ft_atoi_long(split[j]))
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	if (split[0] == 0)
-		return (0);
-	return (1);
-}
-
-char	**parse_input(char **argv)
-{
-	int		i;
-	char	*str;
-	char	**split;
-
-	str = ft_strdup("");
-	i = 1;
-	while (argv[i])
-	{
-		str = ft_strjoin(str, argv[i]);
-		str = ft_strjoin(str, " ");
-		i++;
-	}
-	split = ft_split(str, ' ');
-	if (!is_invalid_input(split))
-	{
-		write(2, "ERROR\n", 6);
-		exit(1);
-	}
-	free(str);
-	return (split);
-}
-
-void	execute_instructions(t_stack *a, t_stack *b)
-{
-	char	*instruction;
-
-	instruction = get_next_line(0);
-	while (instruction)
-	{
 		if (ft_strncmp(instruction, "pa", 2) == 0)
 			pa(a, b, 0);
 		if (ft_strncmp(instruction, "pb", 2) == 0)
@@ -99,40 +24,20 @@ void	execute_instructions(t_stack *a, t_stack *b)
 			sb(b, 0);
 		if (ft_strncmp(instruction, "ss", 2) == 0)
 			ss(a, b, 0);
+}
+
+void	execute_instructions(t_stack *a, t_stack *b)
+{
+	char	*instruction;
+
+	instruction = get_next_line(0);
+	while (instruction)
+	{
+		exec_one_instruction(a, b, instruction);
 		instruction = get_next_line(0);
 	}
 }
 
-void	print_stack(t_stack *stack)
-{
-	t_node	*node;
-
-	if (stack_is_empty(stack))
-		return ;
-	node = stack->top;
-	while (node->bellow)
-	{
-		ft_putnbr_fd(node->num, 1);
-		ft_putstr_fd(" | ", 1);
-		node = node->bellow;
-	}
-	ft_putnbr_fd(node->num, 1);
-	ft_putchar_fd('\n', 1);
-}
-
-int	stack_is_sorted(t_stack *stack)
-{
-	t_node	*node;
-
-	node = stack->top;
-	while (node && node->bellow)
-	{
-		if (node->num > node->bellow->num)
-			return (0);
-		node = node->bellow;
-	}
-	return (1);
-}
 
 int	main(int argc, char **argv)
 {
