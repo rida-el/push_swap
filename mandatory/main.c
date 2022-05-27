7072bc9458d121e6d43f74de7e8618fe6415ef0c
rida el mazary <rida.el99@gmail.com>
2022-05-27 20:09:07 +0100

    check if stack is sorted

--- mandatory/main.c
+++ mandatory/main.c
@@ -1,74 +1,5 @@
 #include "push_swap.h"
 
-int	is_valid_individual_input(char *input)
-{
-	int		i;
-	long	num;
-
-	i = 1;
-	if (!(ft_isdigit(input[0]) || input[0] == '+' || input[0] == '-' ))
-		return (0);
-	while (input[i])
-	{
-		if (!ft_isdigit(input[i]))
-			return (0);
-		i++;
-	}
-	if (i > 11)
-		return (0);
-	num = ft_atoi_long(input);
-	if (num > 2147483647 || num < -2147483648)
-		return (0);
-	return (1);
-}
-
-int	is_invalid_input(char **split)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (split[i])
-	{
-		if (!is_valid_individual_input(split[i]))
-			return (0);
-		j = i + 1;
-		while (split[j])
-		{
-			if (ft_atoi_long(split[i]) == ft_atoi_long(split[j]))
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	if (split[0] == 0)
-		return (0);
-	return (1);
-}
-
-char	**parse_input(char **argv)
-{
-	int		i;
-	char	*str;
-	char	**split;
-
-	str = ft_strdup("");
-	i = 1;
-	while (argv[i])
-	{
-		str = ft_strjoin(str, argv[i]);
-		str = ft_strjoin(str, " ");
-		i++;
-	}
-	split = ft_split(str, ' ');
-	if (!is_invalid_input(split))
-	{
-		write(0, "ERROR\n", 6);
-		exit(1);
-	}
-	free(str);
-	return (split);
-}
 
 int	main(int argc, char **argv)
 {
@@ -90,6 +21,8 @@ int	main(int argc, char **argv)
 		free(split[i]);
 		i++;
 	}
+	if(stack_is_sorted(a))
+		exit(0);
 	push_swap(a, b);
 	free_stack(a);
 	free_stack(b);
