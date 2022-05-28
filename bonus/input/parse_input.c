a5a80fb2be65477171f1ffde9a3b545410981b75
rida el mazary <rida.el99@gmail.com>
2022-05-28 16:09:02 +0100

    bonus - handle extreme cases

--- bonus/input/parse_input.c
+++ bonus/input/parse_input.c
@@ -1,5 +1,38 @@
 #include "../push_swap.h"
 
+int is_space(char c)
+{
+	return (c >= 9 && c <= 13) || c == 32;
+}
+
+int is_all_spaces(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!is_space(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	is_one_sign(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if((str[i] == '-' || str[i] == '+') && !ft_isdigit(str[i+1]))
+			return(1);
+		i++;
+	}
+	return (0);
+}
+
 char	**parse_input(char **argv)
 {
 	int		i;
@@ -10,6 +43,8 @@ char	**parse_input(char **argv)
 	i = 1;
 	while (argv[i])
 	{
+		if (argv[i][0] == '\0' || is_all_spaces(argv[i]) || is_one_sign(argv[i]))
+			exit_error(1);
 		str = ft_strjoin(str, argv[i]);
 		str = ft_strjoin(str, " ");
 		i++;
